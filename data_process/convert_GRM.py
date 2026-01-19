0306a7acee361f36d9241a35b45608cbb1268fb4
jzzhang <jizhe.zhang@outlook.com>
2026-01-19 05:05:16 +0000

    a

--- data_process/convert_GRM.py
+++ data_process/convert_GRM.py
@@ -10,22 +10,23 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     data_source = "general-reasoner"
-    train_dataset = pd.read_parquet("/data0/jzzhang/datasets/RLPR/WebInstruct/rlpr_train.parquet").sample(n=2000, random_state=42)
-    # instruction = "A conversation between User and Assistant. The user asks a question, and the Assistant should solve it step by step and present the final answer in the following format: '\\boxed{X}', where X is the final answer."
-    instruction_following = "Please reason step by step, and put your final answer within \\boxed{}."
+    train_dataset = pd.read_parquet("/data0/jzzhang/datasets/RLPR/WebInstruct/rlpr_train_debug.parquet")
+    instruction = "A conversation between User and Assistant. The user asks a question, and the Assistant should solve it step by step and present the final answer in the following format: '\\boxed{X}', where X is the final answer."
+    # instruction_following = "Please reason step by step, and put your final answer within \\boxed{}."
     # add a row to each data item that represents a unique id
     def make_map_fn(split):
         def process_fn(row):
             idx = row.name
             example = row.to_dict()
             question_raw = example["prompt"][1]['content']
-            question = question_raw + ' ' + instruction_following
+            # question = question_raw + ' ' + instruction_following
             solution = example["reward_model"]["ground_truth"] 
             ability = example["ability"]
             data = {
                 "data_source": data_source,
                 "prompt": [
-                            {"role": "user", "content": question}
+                            {"role":"system","content":instruction},
+                            {"role": "user", "content": question_raw}
                         ],
                 "ability": ability,
                 "reward_model": {"style": "rule", "ground_truth": solution},
